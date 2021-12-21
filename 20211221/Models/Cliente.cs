177e8fe3606bdba4fb44c79df79f41e851521c3b
Miguel Labrador <github4138@mlsdm.com>
2021-12-21 22:32:31 +0100

    Conectada página a db y añadido buscador

---
+++ 20211221/Models/Cliente.cs
@@ -0,0 +1,16 @@
+namespace mvc_ajax.Models
+{
+    public class Cliente
+    {
+        public string Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public Cliente(string Dni, string Nombre, string Apellido)
+        {
+            this.Dni = Dni;
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+        }
+    }
+}
---
+++ 20211221/Models/Persona.cs
@@ -0,0 +1,14 @@
+namespace mvc_ajax.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+
+        public Persona(string Nombre, string Apellidos)
+        {
+            this.Nombre = Nombre;
+            this.Apellidos = Apellidos;
+        }
+    }
+}
